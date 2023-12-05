b597de82b61234464e4c685d73cdfbb1ad5eb61d
David González Ramiro <davidgramiro@gmail.com>
2023-12-05 20:19:52 +0100

    Implementacion en settings

--- TODO/TODO/settings.py
+++ TODO/TODO/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'rest_framework.authtoken',
     'rest_framework',
     'usuarios',
     'tareas'
