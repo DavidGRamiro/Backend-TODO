40ea47b09fd2451b9b6a2652c730973cd813a98f
David González Ramiro <davidgramiro@gmail.com>
2023-12-26 19:23:05 +0100

    Modificaciones en tareas, modelos y serializador. Permisos dependiendo de como se acceda. Cambiar a futuro

--- TODO/tareas/serializers/tarea_serializer.py
+++ TODO/tareas/serializers/tarea_serializer.py
@@ -4,6 +4,11 @@ from rest_framework import serializers
 from tareas.models import Tarea
 
 class TareaSerializer(serializers.ModelSerializer):
+    
+    usuario_asignado = serializers.ReadOnlyField(source='id_fk_usuario.name')
+    categoria = serializers.ReadOnlyField(source="id_fk_categoria.categoria")
+    
+    
     class Meta:
         model = Tarea
         fields = '__all__'
